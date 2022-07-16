b0db20d1e2619211af1b9b1615a9f1f2b9face00
nika <nikaonime@gmail.com>
2022-07-16 11:59:38 +0300

    add new project

---
+++ cool-style/js/app.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+
+setTimeout(function(){
+
+    document.body.classList.add('loaded')
+
+    Draggable.create('.gallery', {
+        bounds: 'body',
+        inertia: true
+    })
+
+})
+
+}
